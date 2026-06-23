c79443c1db229ddeaeff5b80fd352ae98f5fbae0
diegofercri <contact@diegofercri.dev>
2026-06-23 19:01:25 +0200

    chore: quita import redundante de hugeicons en test (llega via barrel)

--- test/atoms_test.dart
+++ test/atoms_test.dart
@@ -1,7 +1,6 @@
 import 'package:atomic_ui/atomic_ui.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hugeicons/hugeicons.dart';
 
 Widget _host(Widget child) => AxApp(home: Center(child: child));
 
